b53392e027a1c96b5998f60ca60a4b0861f7acb4
Kauã Kelvyn <ckaua504@gmail.com>
2025-07-05 20:57:47 -0300

    ajustando sw que estava no formato errado

--- app/public/service-worker.js
+++ app/public/service-worker.js
@@ -2,47 +2,43 @@ const CACHE_NAME = 'supi-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/src/main.jsx',
-  '/App.jsx',
-  '/routes.jsx',
-  '/Profile.jsx',
-  '/logoSupi2.png',
   '/manifest.json',
+  '/logoSupi2.png',
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).catch(() => {
+        if (event.request.destination === 'document') {
+          return caches.match('/index.html');
         }
-        return fetch(event.request);
-      })
+      });
+    })
   );
 });
 
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key);
           }
         })
-      );
-    })
+      )
+    )
   );
 });
